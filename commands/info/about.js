8cd57c33fbc0bffa2b39b0bcbcb7869b1b004041
Sxlitude <95486112+Sxlitude@users.noreply.github.com>
2022-03-31 21:14:17 +0530

    Update about.js

--- commands/info/about.js
+++ commands/info/about.js
@@ -1,4 +1,4 @@
-const { Message, Client, MessageEmbed } = require("discord.js");
+const { Message, Client, MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
 const { Embeds, Links, InteractionEmbeds } = require('../../files/embeds.js');
 
 const Database = require("@replit/database");
@@ -8,7 +8,13 @@ module.exports = {
   name: "about",
   aliases: ['a'],
   run: async (client, message, args) => {
+    const Buttons = new MessageActionRow().addComponents(
+        new MessageButton()
+        .setLabel("GitHub Repo")
+        .setStyle("LINK")
+        .setURL("https://github.com/sxlitude/antinuke")
+        );
     const embed = InteractionEmbeds.Information(client.ws.ping, client.guilds.cache.map((guild) => guild.memberCount).reduce((p, c) => p + c), client.guilds.cache.size)
-    message.channel.send({ embeds: [embed] });
+    message.channel.send({ embeds: [embed], components: [Buttons] });
   },
 };
