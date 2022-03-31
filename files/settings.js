48d8c28a3a079daec02af955bfacb1f541eb22ec
Sxlitude <95486112+Sxlitude@users.noreply.github.com>
2022-03-31 20:10:36 +0530

    Update settings.js

--- files/settings.js
+++ files/settings.js
@@ -1,17 +1,21 @@
 module.exports = {
+  // This works.
   bot: {
     prefix: "=",
     token: process.env.token,
     invite: `https://dsc.gg/antiwizz`
   },
+  // This works.
   options: {
     founders: ["891214041391988757"],
-    privateMode: true
+    privateMode: false
   },
+  // This is useless yet.
   credits: {
     developer: "Sxlitude#8885",
     sourceCode: "https://github.com/sxlitude/antinuke"
   },
+  // This is useles yet.
   emojis: {
     okay: "",
     error: "",
