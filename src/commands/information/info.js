c0894e1b816900be89e001b5540f402e90851526
Sxlitude <95486112+Sxlitude@users.noreply.github.com>
2022-07-06 10:11:32 +0000

    update about.js

--- src/commands/information/info.js
+++ src/commands/information/info.js
@@ -4,6 +4,9 @@ module.exports = {
   name: 'about',
   aliases: ['info', 'botinfo'],
   run: async (client, message, args) => {
+    const everyGuild = client.guilds.cache.map((guild) => guild.memberCount);
+    const userCount = everyGuild.reduce((x, y) => x + y);
+    
     message.reply({
       embeds: [new MessageEmbed()
       .setColor('PURPLE')
@@ -17,7 +20,9 @@ module.exports = {
 **BOT INFO**
 ﹒Owner :: Sxlitude
 ﹒Library :: discord.js
-﹒Latency :: ${client.ws.ping}ms`)]
+﹒Latency :: ${client.ws.ping}ms
+﹒Guilds :: ${client.guilds.cache.size.toLocaleString()}
+﹒Users :: ${userCount.toLocaleString()}`)]
     })
   }
 }
