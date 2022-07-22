4276022be54d9bcf36e87d74e4ba5a6bdb256402
Sxlitude <95486112+Sxlitude@users.noreply.github.com>
2022-07-22 08:40:35 +0000

    updated the about embed in help menu

--- src/commands/information/help.js
+++ src/commands/information/help.js
@@ -39,9 +39,9 @@ module.exports = {
         .setDisabled(true)
         .addOptions([
           {
-            label: 'test',
+            label: 'this menu is disabled',
             value: 'x',
-            description: '?'
+            description: 'do the command again'
           }
           ])
     )
@@ -49,7 +49,7 @@ module.exports = {
     message.channel.send({ embeds: [help], components: [menuOptions] }).then((msg) => {
       setTimeout(() => {
         msg.edit({ components: [disabled] });
-      }, 5000)
+      }, 10000)
     })
 
     const cmds = new MessageEmbed()
@@ -68,7 +68,7 @@ module.exports = {
     const credits = new MessageEmbed()
       .setThumbnail(`${client.user.avatarURL({ dynamic: true })}`)
       .setColor('#2C2F33')
-      .setDescription(`**__credits__**\n﹒shows some bot information\n﹒also tells about the developer\n\n>>> **bot info**\n﹒developer: Sxlitude#8885\n﹒language: node.js\n﹒library: discord.js\n﹒host: heroku\n\n**bot stats**\n﹒users: ${users}\n﹒servers: ${client.guilds.cache.size}\n﹒ping: ${client.ws.ping}ms`)
+      .setDescription(`**__credits__**\n﹒shows some bot information\n﹒also tells about the developer\n\n>>> **bot info**\n﹒developer: Sxlitude#8885\n﹒database: mongo DB\n﹒language: node.js\n﹒library: discord.js\n﹒host: heroku\n\n**bot stats**\n﹒users: ${users}\n﹒servers: ${client.guilds.cache.size}\n﹒ping: ${client.ws.ping}ms\n\n**contributors**\n﹒~ Piyush#1972 \n﹒ⴽΛ 𝐉𝐚𝐧𝐞𝐭#6908\n﹒shadowTW#7100\n﹒comy#0001\n﹒troubled#1337\n﹒颢烨 hy#1000\n﹒S.mode#9723\n﹒! 𝐍σт𝐘συя𝓥𝞪η𝓢♄♔🥀†ʰ𝕔#9999\n﹒𝑺𝑻丶༒𝐃𝐄𝐑𝐄𝐊༒⸸ᴳᵀ#3081\n\n**;** made with :heart: by Sxlitude & the contributors.`)
 
     const filter = (i) => i.isSelectMenu();
     const collector = message.channel.createMessageComponentCollector({ filter, limit: 10 });
